503e81a152cfdd5a105a789963ad334566f17718
sarojajeet <sarojajeet660@gmail.com>
2025-08-20 19:30:44 +0530

    package .json change

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,22 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from "@tailwindcss/vite";
+
+// export default defineConfig({
+//   plugins: [react(),tailwindcss()],
+//   server: {
+//     port: 8080,              // or 5173 if you prefer
+//     host: true,              // allow connections from outside container
+//     allowedHosts: [
+//       'squid-app-l3ltb.ondigitalocean.app',  // DigitalOcean ingress host
+//     ],
+//   },
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  server: {
-    port: 8080,              // or 5173 if you prefer
-    host: true,              // allow connections from outside container
-    allowedHosts: [
-      'squid-app-l3ltb.ondigitalocean.app',  // DigitalOcean ingress host
-    ],
-  },
+  plugins: [react(), tailwindcss()],
 })
