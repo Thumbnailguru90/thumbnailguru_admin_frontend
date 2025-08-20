f4a92a65a06a880bb8041e76cd6a3027bc062df5
sarojajeet <sarojajeet660@gmail.com>
2025-08-20 18:12:34 +0530

    vite file change

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: [
+      'squid-app-l3ltb.ondigitalocean.app', // 👈 add your host here
+    ],
+  },
 })
