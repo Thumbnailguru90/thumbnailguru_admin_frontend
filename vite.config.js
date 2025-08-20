299cbc233b88ced75843fc7220bd2e7e48ed4fcb
Shashiverma9042 <101035907+Thumbnailguru90@users.noreply.github.com>
2025-08-20 17:58:33 +0530

    Update vite.config.js
    update port

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 8080, // run Vite dev server on 8080
+    host: true, // listen on all network interfaces (needed in containers)
+  },
 })
