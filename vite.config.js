c0f73bc22b32c91c0fa7cae6a13b25c2df424170
Shashiverma9042 <101035907+Thumbnailguru90@users.noreply.github.com>
2025-08-20 18:13:58 +0530

    Update vite.config.js
    update url

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 8080,   // match Kubernetes probe
-    host: true,   // required inside container
+    port: 8080,              // or 5173 if you prefer
+    host: true,              // allow connections from outside container
+    allowedHosts: [
+      'squid-app-l3ltb.ondigitalocean.app',  // DigitalOcean ingress host
+    ],
   },
 })
